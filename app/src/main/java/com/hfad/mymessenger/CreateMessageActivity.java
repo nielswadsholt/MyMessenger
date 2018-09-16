e93f0e931b25baa640bd88bfcd39cc3084e49323
Niels Wadsholt <eaaniwa@students.eaaa.dk>
2018-09-16 20:53:09 +0200

    Initial commit

---
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,35 @@
+package com.hfad.mymessenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        EditText messageView = findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        // First implementation: Send from one activity to another with explicit intent
+        /*Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);*/
+
+        // Improved version: Let user choose app to handle message
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
+    }
+}
